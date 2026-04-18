b7d8dbe76013914a79fcfbbc61c4e50934cb9d26
Mahesh Bogar <bogarmahesh9@gmail.com>
2026-04-18 23:11:22 +0530

    Add ReverseString class with reverse method
    Implement a method to reverse a character array in place.

---
+++ Day-9/Reverse-String.java
@@ -0,0 +1,28 @@
+class ReverseString {
+
+    public static void reverse(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        // Two pointer approach
+        while(left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h','e','l','l','o'};
+
+        reverse(s);
+
+        // Print result
+        System.out.println(s);
+    }
+}
