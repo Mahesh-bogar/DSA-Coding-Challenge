1a557acfe79a7a790a75c3e2ee7b3359a2bb8785
Mahesh Bogar <bogarmahesh9@gmail.com>
2026-04-29 22:59:54 +0530

    Add ValidAnagram class to check for anagrams

---
+++ Day-14/ValidAnagram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+}
