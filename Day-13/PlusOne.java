7af2c8cefe9b393133c36a59bc4ee761c1cb670f
Mahesh Bogar <bogarmahesh9@gmail.com>
2026-04-29 22:56:41 +0530

    Add PlusOne class to handle incrementing digits
    Implement a method to add one to an integer represented as an array of digits.

---
+++ Day-13/PlusOne.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class PlusOne {
+    public static int[] plusOne(int[] digits) {
+
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9,9,9};
+        System.out.println(Arrays.toString(plusOne(arr)));
+    }
+}
